725f5410e39048e13297bbfc7b67ada1cd6c74a0
Taras-Roboteckyi <rtvr@ukr.net>
2022-01-22 23:00:09 +0200

    Додав код в App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,67 @@
 import { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Searchbar from './components/Searchbar/Searchbar';
 import ImageGallery from './components/ImageGallery/ImageGallery';
 import Modal from './components/Modal/Modal';
+import Button from './components/Button/Button';
+import Loader from './components/Loader/Loader';
+
+import API from './services/pixabayservices';
+
+const newsApi = new API();
 
 export default class App extends Component {
   state = {
     pictureName: '',
     pictureModal: null,
     showModal: false,
+    picture: null,
+    loading: false,
+    error: null,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevName = prevState.pictureName;
+    const nextName = this.state.pictureName;
+    if (nextName !== prevName) {
+      this.setState({ loading: true, picture: null });
+
+      newsApi.query = nextName;
+      newsApi.resetPage();
+      newsApi
+        .fetchImages()
+        .then(({ hits }) => {
+          this.setState({ picture: hits });
+
+          if (hits.length === 0) {
+            this.setState({ picture: null });
+            toast.error('Sorry, there are no images matching your search query. Please try again.');
+            return;
+          }
+        })
+        .catch(error => this.setState({ error }))
+        .finally(() => this.setState({ loading: false }));
+    }
+  }
+
+  fetchMorePictures = () => {
+    const { picture } = this.state;
+
+    this.setState({ loading: true });
+    return newsApi
+      .fetchImages()
+      .then(({ hits }) => {
+        this.setState({ picture: [...picture, ...hits] });
+        if (hits.length === 0) {
+          toast.error('Sorry, there are no more images matching your search.');
+          return;
+        }
+      })
+      .catch(error => this.setState({ error }))
+      .finally(() => this.setState({ loading: false }));
   };
 
   formSubmitHandler = data => {
@@ -26,12 +78,17 @@ export default class App extends Component {
   };
 
   render() {
-    const { pictureName, showModal, pictureModal } = this.state;
+    const { showModal, pictureModal } = this.state;
+    const { picture, loading } = this.state;
 
     return (
       <div>
         <Searchbar formSubmit={this.formSubmitHandler}></Searchbar>
-        <ImageGallery pictureName={pictureName} onClick={this.toggleModal}></ImageGallery>
+
+        {picture && <ImageGallery picture={picture} onClick={this.toggleModal}></ImageGallery>}
+
+        {picture && <Button pagination={this.fetchMorePictures}></Button>}
+        {loading && <Loader></Loader>}
 
         {showModal && (
           <Modal onClose={this.toggleModal}>
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,85 +1,30 @@
-import { Component } from 'react';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import PropTypes from 'prop-types';
 
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+
 import styles from '../ImageGallery/ImageGallery.module.css';
-import API from '../../services/pixabayservices';
-import Button from '../Button/Button';
-import Loader from '../Loader/Loader';
 
-const newsApi = new API();
+const { ImageGallery } = styles;
 
-export default class ImageGallery extends Component {
-  state = {
-    picture: null,
-    loading: false,
-    error: null,
-  };
+const ImageGalleryPictures = ({ picture, onClick }) => {
+  return (
+    <ul className={ImageGallery}>
+      {picture.map(({ largeImageURL, webformatURL, tags }, index) => {
+        return (
+          <ImageGalleryItem
+            key={index}
+            data={{ largeImageURL, webformatURL, tags }}
+            largePicture={onClick}
+          />
+        );
+      })}
+    </ul>
+  );
+};
 
-  componentDidUpdate(prevProps, prevState) {
-    const prevName = prevProps.pictureName;
-    const nextName = this.props.pictureName;
-    if (nextName !== prevName) {
-      this.setState({ loading: true, picture: null });
+export default ImageGalleryPictures;
 
-      newsApi.query = nextName;
-      newsApi.resetPage();
-      newsApi
-        .fetchImages()
-        .then(({ hits }) => {
-          this.setState({ picture: hits });
-          if (hits.length === 0) {
-            this.setState({ picture: null });
-            toast.error('Sorry, there are no images matching your search query. Please try again.');
-            return;
-          }
-          this.props.largestPicture(this.state.picture);
-        })
-        .catch(error => this.setState({ error }))
-        .finally(() => this.setState({ loading: false }));
-    }
-  }
-
-  fetchMorePictures = () => {
-    const { picture } = this.state;
-
-    this.setState({ loading: true });
-    return newsApi
-      .fetchImages()
-      .then(({ hits }) => {
-        this.setState({ picture: [...picture, ...hits] });
-        if (hits.length === 0) {
-          toast.error('Sorry, there are no more images matching your search.');
-          return;
-        }
-        this.props.largestPicture(this.state.picture);
-      })
-      .catch(error => this.setState({ error }))
-      .finally(() => this.setState({ loading: false }));
-  };
-
-  render() {
-    const { picture, loading } = this.state;
-    const { ImageGallery } = styles;
-
-    return (
-      <>
-        <ul className={ImageGallery}>
-          {picture &&
-            picture.map(({ largeImageURL, webformatURL, tags }, index) => {
-              return (
-                <ImageGalleryItem
-                  key={index}
-                  data={{ largeImageURL, webformatURL, tags }}
-                  largePicture={this.props.onClick}
-                />
-              );
-            })}
-        </ul>
-        {picture && <Button pagination={this.fetchMorePictures}></Button>}
-        {loading && <Loader></Loader>}
-      </>
-    );
-  }
-}
+ImageGalleryPictures.propTypes = {
+  picture: PropTypes.arrayOf(PropTypes.shape).isRequired,
+  onClick: PropTypes.func.isRequired,
+};
